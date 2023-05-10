ea4ecdb4c7fe209f65bed887d5cf01531980119d
bArter <1285434016@qq.com>
2023-05-10 17:01:04 +0800

    封装websocket

---
+++ weboskcet/app.js
@@ -0,0 +1,38 @@
+const WebSocket = require("ws")
+const server = new WebSocket.Server({ port: 8000 })
+server.on('connection', handleConnection)
+function handleConnection(ws) {
+    console.log("Connection")
+    ws.on('close', handleClose)
+    ws.on('error', handleError)
+    ws.on('message', handleMessage)
+}
+
+function handleClose() {
+    console.log("server close")
+    this.send({
+        mode: "Message",
+        msg: "--Server closed"
+    })
+}
+function handleError(e) {
+    console.log("server error" + e)
+}
+function handleMessage(data) {
+    const { mode, msg } = JSON.parse(data)
+
+    switch (mode) {
+        case "MESSAGE":
+             console.log("User message")
+            this.send(JSON.stringify(JSON.parse(data)))
+            break;
+        case "HEAT_BEAT":
+            console.log("HEAT_BEAT")
+            this.send(JSON.stringify(JSON.parse(data)))
+            break;
+
+        default:
+            break;
+    }
+}
+
